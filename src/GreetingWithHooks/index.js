fa9c63e2651b257fccfd0c494286c2c24dd7fb6d
Apoorv Kansal <akansal@zendesk.com>
2019-08-23 16:48:35 +1000

    With hooks state exp

--- src/GreetingWithHooks/index.js
+++ src/GreetingWithHooks/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "Box";
 import Header from "Header";
+import Main from "Main";
+import Row from "Row";
 
 const GreetingWithHooks = () => {
+  const [val, setVal] = useState("");
+
+  const handleOnChange = e => {
+    setVal(e.target.value);
+  };
+
   return (
     <Box>
       <Header> Greeting with Hooks </Header>
+      <Main>
+        <Row>
+          <input type="text" val={val} onChange={handleOnChange} />
+        </Row>
+        {val ? <Row>The value: {val} </Row> : null}
+      </Main>
     </Box>
   );
 };

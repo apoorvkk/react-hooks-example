e3d238de77ba778ed2ecb603b24a48c07f8b8448
Apoorv Kansal <akansal@zendesk.com>
2019-08-23 16:48:28 +1000

    Without hooks state exp

--- src/GreetingWithoutHooks/index.js
+++ src/GreetingWithoutHooks/index.js
@@ -3,15 +3,35 @@ import Box from "Box";
 import Header from "Header";
 import Main from "Main";
 import Row from "Row";
-import LabelField from "LabelField";
 
 class GreetingWithoutHooks extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      val: ""
+    };
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      val: e.target.value
+    });
+  };
+
   render() {
     return (
       <Box>
         <Header>Greeting without Hooks</Header>
         <Main>
-          <Row>My row</Row>
+          <Row>
+            <input
+              type="text"
+              val={this.state.val}
+              onChange={this.handleOnChange}
+            />
+          </Row>
+          {this.state.val ? <Row>The value: {this.state.val} </Row> : null}
         </Main>
       </Box>
     );
